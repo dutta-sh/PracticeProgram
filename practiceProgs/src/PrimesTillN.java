02f08b1f1fd64da614c1663902eac089b05038b5
duttash <shouvik.qwerty.dutta@gmail.com>
2019-06-03 18:10:06 -0400

    added more practice

---
+++ practiceProgs/src/PrimesTillN.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class PrimesTillN {
+
+    private static void printPrimesTillN(int n) {
+        boolean prime[] = new boolean[n+1];
+        Arrays.fill(prime, true);
+
+        prime[0] = false;
+        prime[1] = false;
+
+        for(int i = 2; i*i <= n; i++) {
+            if(prime[i]) {
+                int j = 2*i;
+                while(j <= n) {
+                    prime[j] = false;
+                    j += i;
+                }
+            }
+        }
+
+        int ct = 0;
+        for(int i = 1; i <=n; i++) {
+            if(prime[i]) {
+                ct++;
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println("\r\nTotal primes: " + ct);
+    }
+
+    public static void main(String[] args) {
+        printPrimesTillN(400000);
+    }
+}
