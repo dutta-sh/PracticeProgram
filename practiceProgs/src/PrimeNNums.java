7f3479011d7848875c4d7d8e5baea57cd7ab2435
duttash <shouvik.qwerty.dutta@gmail.com>
2019-06-21 11:19:42 -0400

    more practice

--- practiceProgs/src/PrimeNNums.java
+++ practiceProgs/src/PrimeNNums.java
@@ -6,13 +6,11 @@ public class PrimeNNums {
     private static List<Integer> primes = new ArrayList<>();
 
     private static void getNPrimes(int n) {
-        primes.add(2);
-
         int nextNum = 3;
         while(primes.size() < n) {
             boolean isPrime = true;
+            int checkUpto = (int)Math.floor(Math.sqrt(nextNum));
             for(int prime : primes) {
-                int checkUpto = (int)Math.floor(Math.sqrt(nextNum));
                 if(prime <= checkUpto && nextNum % prime == 0) {
                 //if(nextNum % prime == 0) {
                     isPrime = false;
@@ -26,30 +24,34 @@ public class PrimeNNums {
         }
     }
 
+    private static List<Integer> primesRec = new ArrayList<>();
+
     private static void getNPrimesRec(int n, int nextNum) {
-        if(primes.size() == n)
+        if(primesRec.size() == n)
             return;
 
         boolean isPrime = true;
-        for(int prime : primes) {
-            int checkUpto = (int)Math.floor(Math.sqrt(nextNum));
+        int checkUpto = (int)Math.floor(Math.sqrt(nextNum));
+        for(int prime : primesRec) {
             if(prime <= checkUpto && nextNum % prime == 0) {
                 isPrime = false;
                 break;
             }
         }
         if(isPrime) {
-            primes.add(nextNum);
+            primesRec.add(nextNum);
         }
 
         getNPrimesRec(n, nextNum + 2);
     }
 
     public static void main(String[] args) {
+        primes.add(2);
         getNPrimes(20);
-
-        //primes.add(2);
-        //getNPrimesRec(20, 3);
         System.out.println(primes);
+
+        primesRec.add(2);
+        getNPrimesRec(20, 3);
+        System.out.println(primesRec);
     }
 }
