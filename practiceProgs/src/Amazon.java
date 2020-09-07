78d566e82a37944581face5c145d31d660c20c03
Paromita <paromita.newid@gmail.com>
2020-09-07 17:28:50 -0400

    tech screen 2020/09

---
+++ practiceProgs/src/Amazon.java
@@ -0,0 +1,119 @@
+
+import java.util.*;
+
+public class Amazon {
+
+    public static List<String> foo(String inputStr, int num) {
+        // Write your code here
+        int len = inputStr.length();
+        List<String> response = new ArrayList<>();
+        for(int i = 0; i <= len - num; i++) {
+            String sub = inputStr.substring(i, i + num);    //sliding window string
+
+            Set<Character> charSet = new HashSet<>();
+            boolean charFoundTwice = false;                   //stores if exactly one char is present twice
+            for(Character c : sub.toCharArray()) {
+                if(!charSet.contains(c)) {       //add this char - this is new
+                    charSet.add(c);
+                } else if(charFoundTwice && charSet.contains(c)) {   //another char is already present twice
+                    charFoundTwice = false;
+                    break;
+                } else if(!charFoundTwice && charSet.contains(c)) {   //this char is present twice
+                    charFoundTwice = true;
+                }
+            }
+
+            if(charFoundTwice) {
+                if(!response.contains(sub))
+                    response.add(sub);
+            }
+        }
+
+        Collections.sort(response);
+
+        return response;
+    }
+
+    public static void main(String[] args) {
+        List<String> s1 = Arrays.asList(new String[]{"a", "b"});
+        List<String> s2 = Arrays.asList(new String[]{"d", "e"});
+        List<String> s3 = Arrays.asList(new String[]{"c", "e"});
+        List<String> s4 = Arrays.asList(new String[]{"f", "g", "c"});
+
+        List<List<String>> list = new ArrayList<>();
+        list.add(s1); list.add(s2); list.add(s3); list.add(s4);
+
+        System.out.println(foo(list));
+
+    }
+
+    static class MyNode {
+        String s;
+        Set<MyNode> neighbors = new HashSet<>();
+
+        public MyNode(String s) {
+            this.s = s;
+        }
+
+        public void addNeighbor(MyNode n) {
+            neighbors.add(n);
+        }
+
+        public String toString() {
+            String nei = "";
+            for(MyNode n : neighbors) {
+                nei += n.s + " ";
+            }
+            return (s + " -> " + nei);
+        }
+    }
+
+    public static List<String> foo(List<List<String>> itemAssociation) {
+        //generate the graph
+        Map<String, MyNode> graph = new HashMap<>();
+        Set<String> visited = new HashSet<>();
+        for(List<String> items : itemAssociation) {
+            int size = items.size();
+            for(int i = 0; i < size; i++) {
+                MyNode node = graph.getOrDefault(items.get(i), new MyNode(items.get(i)));
+                graph.put(items.get(i), node);
+
+                if(i != size - 1) {
+                    MyNode n1 = graph.getOrDefault(items.get(i + 1), new MyNode(items.get(i + 1)));
+                    node.addNeighbor(n1);
+                    n1.addNeighbor(node);
+                    graph.put(items.get(i + 1), n1);
+                }
+
+                if(i != 0) {
+                    MyNode n2 = graph.getOrDefault(items.get(i - 1), new MyNode(items.get(i - 1)));
+                    node.addNeighbor(n2);
+                    n2.addNeighbor(node);
+                    graph.put(items.get(i - 1), n2);
+                }
+            }
+        }
+        System.out.println(graph);
+
+        Set<String> longest = new TreeSet<>();
+        for(String s : graph.keySet()) {
+            Set<String> thisVisit = new TreeSet<>();
+            dfs(graph.get(s), visited, thisVisit);
+
+            if(thisVisit.size() > longest.size())
+                longest = thisVisit;
+        }
+
+        return new ArrayList(longest);
+    }
+
+    public static void dfs(MyNode n, Set<String> visited, Set<String> thisVisit) {
+        if(!visited.contains(n.s)) {
+            visited.add(n.s);
+            thisVisit.add(n.s);
+            for(MyNode neighbor : n.neighbors) {
+                dfs(neighbor, visited, thisVisit);
+            }
+        }
+    }
+}
